209e0b32b5c311c7b0786b42910fbaca627f51f4
Anthony <90657932+frostysmags@users.noreply.github.com>
2023-02-14 12:14:09 -0500

    Updated popup.js and background.js
    popup.js - added functionality to allow audio queues for new tickets
    background.js - added functionality to play audio during event changes on page refresh

--- background.js
+++ background.js
@@ -1,20 +1,42 @@
+let tickets = -2
+
 function startInterval() {
-    let interval = setInterval(() => {
-        chrome.tabs.query({active:true}, function(tabs){
-            if (localStorage.getItem('isToggled') === 'true') {
-                console.log("refreshing")
-                chrome.tabs.executeScript(tabs[0].id, {
-                    code: "document.querySelector('[data-test-id=views_views-list_header-refresh]').click();"
-                });
-            }
-        });
-    }, 10000)
+  let interval = setInterval(() => {
+      chrome.tabs.query({active:true}, function(tabs){
+          if (localStorage.getItem('isToggled') === 'true') {
+            chrome.tabs.executeScript(tabs[0].id, {
+                code: `
+                chrome.runtime.sendMessage({ action: "LOG_INFO", viewCount: document.querySelector('a[class="sc-1s8ami4-3 VxCKy"] div[data-test-id="views_views-list_row_count"]').innerHTML }, function(response) {});
+                document.querySelector('[data-test-id=views_views-list_header-refresh]').click();
+                `
+            });
+          }
+      });
+  }, 10000)
+}
+
+function logInfo(viewCount) {
+  if (tickets === -2) {
+    tickets = parseInt(viewCount)
+  }
+  if (parseInt(viewCount) < tickets && localStorage.getItem('taken_checkboxChecked')) {
+    let sound = new Audio('aud-taken-tik.wav');
+    sound.play();
+    tickets = parseInt(viewCount)
+  }
+  if (parseInt(viewCount) > tickets && localStorage.getItem('new_checkboxChecked')) {
+    let sound = new Audio('aud-new-tik.wav');
+    sound.play();
+    tickets = parseInt(viewCount)
+  }
 }
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message === 'startInterval') {
-      startInterval();
-    }
+function(request, sender, sendResponse) {
+  if (request.message === 'startInterval') {
+    startInterval();
   }
-);
+  if (request.action === "LOG_INFO") {
+    logInfo(request.viewCount)
+  }
+});
--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// RELOAD BUTTON CLICK
 document.getElementById('reloadButton').addEventListener('click', function(){
     chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
         chrome.tabs.executeScript(tabs[0].id, {
@@ -26,6 +27,53 @@ document.querySelector('[data-toggle="toggle"]').addEventListener('click', funct
     } else {
         this.style.backgroundColor = 'rgb(56, 146, 56)';
         localStorage.setItem('isToggled', 'true');
-        chrome.runtime.sendMessage({message: 'startInterval'});
+        chrome.runtime.sendMessage({message: 'startInterval', new_checkboxChecked: new_checkboxChecked, taken_checkboxChecked: taken_checkboxChecked});
     }
 });
+
+const new_checkbox = document.querySelector('input[name="new_checkbox"]');
+let new_checkboxChecked = false;
+const taken_checkbox = document.querySelector('input[name="taken_checkbox"]');
+let taken_checkboxChecked = false;
+
+new_checkbox.addEventListener('change', () => {
+if (new_checkbox.checked) {
+    localStorage.setItem('new_checkboxChecked', true);
+    new_checkboxChecked = true;
+    console.log('checked');
+} else {
+    localStorage.setItem('new_checkboxChecked', false);
+    new_checkboxChecked = false;
+}
+});
+
+// Check if the new_checkbox has been checked before
+if (localStorage.getItem('new_checkboxChecked') === 'true') {
+new_checkbox.checked = true;
+new_checkboxChecked = true;
+console.log('checked');
+} else {
+new_checkbox.checked = false;
+new_checkboxChecked = false;
+}
+
+taken_checkbox.addEventListener('change', () => {
+if (taken_checkbox.checked) {
+    localStorage.setItem('taken_checkboxChecked', true);
+    taken_checkboxChecked = true;
+    console.log('checked');
+} else {
+    localStorage.setItem('taken_checkboxChecked', false);
+    taken_checkboxChecked = false;
+}
+});
+
+// Check if the taken_checkbox has been checked before
+if (localStorage.getItem('taken_checkboxChecked') === 'true') {
+taken_checkbox.checked = true;
+taken_checkboxChecked = true;
+console.log('checked');
+} else {
+taken_checkbox.checked = false;
+taken_checkboxChecked = false;
+}
