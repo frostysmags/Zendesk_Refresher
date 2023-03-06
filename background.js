b432fab3dc16e7f2be05e5c3658772a6d412627d
Anthony <90657932+frostysmags@users.noreply.github.com>
2023-03-06 09:40:21 -0500

    Updated background.js
    optimized the Interval to detect whether or not zendesk is the active tab and run only under that condition

--- background.js
+++ background.js
@@ -3,14 +3,18 @@ let tickets = -2
 function startInterval() {
   let interval = setInterval(() => {
       chrome.tabs.query({active:true}, function(tabs){
-          if (localStorage.getItem('isToggled') === 'true') {
-            chrome.tabs.executeScript(tabs[0].id, {
-                code: `
-                chrome.runtime.sendMessage({ action: "LOG_INFO", viewCount: document.querySelector('a[data-view-id="360111822573"] div[data-test-id="views_views-list_row_count"]').innerHTML }, function(response) {});
-                document.querySelector('[data-test-id=views_views-list_header-refresh]').click();
-                `
-            });
+        if (tabs && tabs[0].url) {
+          if (tabs[0].url.indexOf('ensurem.zendesk.com') > -1) {
+            if (localStorage.getItem('isToggled') === 'true') {
+              chrome.tabs.executeScript(tabs[0].id, {
+                  code: `
+                  chrome.runtime.sendMessage({ action: "LOG_INFO", viewCount: document.querySelector('a[data-view-id="360111822573"] div[data-test-id="views_views-list_row_count"]').innerHTML }, function(response) {});
+                  document.querySelector('[data-test-id=views_views-list_header-refresh]').click();
+                  `
+              });
+            }
           }
+        }
       });
   }, 10000)
 }
